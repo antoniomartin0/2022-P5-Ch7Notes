efb5563416187289e40d91ae37dc634dc6a3d360
Antonio Martin <antonio.martin@vtsd.com>
2022-12-01 11:43:06 -0500

    Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,2 +1,16 @@
-public class Calculator {
+import java.util.Scanner;
+public class Calculator {   public static int addNums(){
+    System.out.println("We are going to add two numbers");
+    System.out.println("Please enter them below");
+
+    Scanner scan = new Scanner(System.in);
+    int num1 = scan.nextInt();
+    int num2 = scan.nextInt();
+
+    return (num1 + num2);
+
+}
+
+
+
 }
--- VoidReturn.java
+++ VoidReturn.java
@@ -1,21 +1,10 @@
 import java.util.Scanner;
 public class VoidReturn {
     public static void main(String args[]){
-        int total = addNums();
 
-        System.out.println("Your total again was: " + total);
+
+        System.out.println("Your total again was: " + Calculator.addNums());
     }
 
-    public static int addNums(){
-        System.out.println("We are going to add two numbers");
-        System.out.println("Please enter them below");
-
-        Scanner scan = new Scanner(System.in);
-        int num1 = scan.nextInt();
-        int num2 = scan.nextInt();
-
-        return (num1 + num2);
-
-    }
 
 }
